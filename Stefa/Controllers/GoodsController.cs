00425afd901760c9a8440d2f579c575a7da266ab
ViMi <dneprtaugmail.com>
2022-06-16 01:45:09 +0300

    init

---
+++ Stefa/Controllers/GoodsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Linq;
+
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Stefa.Controllers
+{
+    public class GoodsController : Controller
+    {
+
+        [HttpGet]
+        async public Task<IActionResult> Get()
+        {
+            return NoContent();
+        }
+
+
+        [HttpPost]
+        async public Task<IActionResult> AddNewGoods()
+        {
+            return NoContent();
+        }
+
+
+        [HttpPut]
+        async public Task<IActionResult> Put()
+        {
+            return NoContent();
+        }
+    }
+}
