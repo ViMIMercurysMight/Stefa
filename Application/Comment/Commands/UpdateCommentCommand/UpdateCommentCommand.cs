2e131d85cf3ab5ac1a479ad11f03134fedaf2a11
ViMi <dneprtaugmail.com>
2022-06-16 01:44:31 +0300

    Added app layer

---
+++ Application/Comment/Commands/UpdateCommentCommand/UpdateCommentCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Comment.Commands.UpdateCommentCommand
+{
+    internal class UpdateCommentCommand
+    {
+    }
+}
---
+++ Application/Goods/Queries/GetGoodQuery/GetGoodQuery.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Goods.Queries.GetGoodQuery
+{
+    internal class GetGoodQuery
+    {
+    }
+}
