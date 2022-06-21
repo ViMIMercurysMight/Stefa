c7edd3f4fa4819ad8cab5bc6243892297d969bdc
ViMi <dneprtaugmail.com>
2022-06-22 02:13:05 +0300

    upd main view

--- Stefa/ClientApp/stefa-app/src/components/main/Main.vue
+++ Stefa/ClientApp/stefa-app/src/components/main/Main.vue
@@ -1,22 +1,24 @@
 ﻿<template>
   <v-container>
-    <v-row class="text-center">
-      <v-col cols="12">
-        <v-img
-          :src="require('../../assets/img/stefa2.jpg')"
-          class="my-3"
-          contain
-          height="200"
-        />
+    <v-row>
+      <v-col cols="8" class="mx-auto">
+        <v-carousel cycle :show-arrows="false" :height="'80vh'">
+          <v-carousel-item src="../../assets/img/stefa1.jpg"></v-carousel-item>
+          <v-carousel-item src="../../assets/img/stefa2.jpg"></v-carousel-item>
+          <v-carousel-item src="../../assets/img/stefa3.jpg"></v-carousel-item>
+        </v-carousel>
       </v-col>
 
-      <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">Your etno wave</h1>
-
-        <p class="subheading font-weight-regular">
-          Stefa - is e-shop of moder etno-arts
-          <br />Find your best look
-        </p>
+      <v-col class="text-center">
+        <v-card :height="'100%'">
+          <v-card-text>
+            <h1 class="display-2 font-weight-bold mb-3">Your etno wave</h1>
+            <p class="subheading font-weight-regular">
+              Stefa - is e-shop of moder etno-arts
+              <br />Find your best look
+            </p>
+          </v-card-text>
+        </v-card>
       </v-col>
     </v-row>
   </v-container>
@@ -25,8 +27,13 @@
 <script lang="ts">
 import Vue from "vue";
 import Component from "vue-class-component";
+import Carousel from "../common/Carousel.vue";
 
-@Component
+@Component({
+  components: {
+    Carousel,
+  },
+})
 export default class Main extends Vue {
   constructor() {
     super();
