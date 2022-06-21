6b8e5326fbaf2bfca0294f15099ac1fa06f3e585
ViMi <dneprtaugmail.com>
2022-06-22 02:12:41 +0300

    setted background color at app component & small fixes

--- Stefa/ClientApp/stefa-app/src/App.vue
+++ Stefa/ClientApp/stefa-app/src/App.vue
@@ -1,32 +1,24 @@
 <template>
-  <v-app>
+  <v-app style="background: #f9f9f9">
     <Header></Header>
     <v-main>
       <router-view />
     </v-main>
-    <Footer></Footer>
   </v-app>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import Footer from "../src/components/common/Footer.vue";
 import Header from "../src/components/common/Header.vue";
 export default Vue.extend({
   name: "App",
 
   components: {
-    Footer,
     Header,
   },
   data: () => ({
     //      font-family: "Water Brush", cursive;
+    //:style="{ background: $vuetify.theme.themes[theme].background }"
   }),
 });
 </script>
-<style>
-.stefa-header-text {
-  font-family: "Estonia", cursive;
-  font-size: 64px;
-}
-</style>
